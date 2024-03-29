07bf715cc34fff4cd4f649c6f76fa027aea71a29
Aftab Shaikh <sk.aftab1999@gmail.com>
2024-03-29 23:25:01 +0530

    Rushikesh | Initial Commit

---
+++ src/test/java/com/techninja/qnabackend/QnaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techninja.qnabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QnaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
